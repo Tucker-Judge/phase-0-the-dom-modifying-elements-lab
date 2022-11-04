140fbcf0c1c34134aaa59bfb224f4707dc1e1837
Tucker <tudge669@gmail.com>
2022-11-04 15:15:09 -0500

    commit tree f53060bba5a343b32d76456db25150bb200af011

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const box = document.getElementById("main");
+box.remove();
+const newHeader = document.createElement('h1')
+//document.getElementById("newHeader").id = ""
+newHeader.textContent = "Tucker is the champion"
+newHeader.id = "victory"
